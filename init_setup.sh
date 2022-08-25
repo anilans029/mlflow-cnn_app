e48a977443740ce21de8c14473772f2f29ca34fa
anil <anilsai029@gmail.com>
2022-08-25 12:23:21 +0530

    updated

--- init_setup.sh
+++ init_setup.sh
@@ -1,9 +1,10 @@
 echo [$(date)]: "START"
 export _VERSION_=3.7
 echo [$(date)]: "creating environment with python ${_VERSION_}"
-conda create --prefix ./env python=${_VERSION_} -y
+# conda create --prefix ./env python=${_VERSION_} -y
+conda create -n cnn_mlflow_env python=${_VERSION_} -y
 echo [$(date)]: "activate environment"
-source activate ./env
+source activate cnn_mlflow_env
 echo [$(date)]: "install requirements"
 pip install -r requirements.txt
 echo [$(date)]: "export conda environment"
