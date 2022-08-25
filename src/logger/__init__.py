6ffaa2b47817647340e57b28c14fb3554dfe3d10
anil <anilsai029@gmail.com>
2022-08-25 12:52:41 +0530

    logger module added

---
+++ src/logger/__init__.py
@@ -0,0 +1,20 @@
+import logging
+from datetime import datetime
+import os
+import pandas as pd
+
+
+LOG_DIR = "running_logs"
+CURRENT_TIME_STAMP = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S') }"
+LOG_FILE_NAME = F"log_{CURRENT_TIME_STAMP}.log"
+LOG_FILE_PATH = os.path.join(LOG_DIR,LOG_FILE_NAME)
+
+
+os.makedirs(LOG_DIR,exist_ok=True)
+logging.basicConfig(filename=LOG_FILE_PATH,
+                level=logging.INFO,
+                filemode="w",
+                format= "[%(asctime)s---%(name)s] %(module)s (%(levelname)s)  ===> %(message)s")
+
+
+
